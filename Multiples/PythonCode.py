44b8c683dc14c96b0773eca1c86a396120a3b3a1
Michael Morey <73544158+MikeNoMorey@users.noreply.github.com>
2022-08-12 20:26:30 -0400

    Add files via upload

---
+++ Multiples/PythonCode.py
@@ -0,0 +1,37 @@
+import re
+import string
+
+#def printsomething():
+#    print("Hello from python!")
+
+#def PrintMe(v):
+#    print("You sent me: " + v)
+#    return 100;
+
+#Square the number sent from c++ assigned to num
+def SquareValue(num):
+
+    #Print the number being squared
+    print("\n", num, " * ", num, " = ", num * num, "\n")
+
+    return
+
+#Create a times table using the number sent from c++ 
+def TimesTable(num):
+
+    #Initial loop number
+    i = 1
+
+    print("\n Displaying the times table... \n")
+
+    #Loop from 1-10
+    while i <= 10:
+
+        #Print the times table
+        print(num, " * ", i, " = ", num * i)
+        i = i + 1
+
+    return
+
+
+
